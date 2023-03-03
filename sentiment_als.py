d34bc3887afa929a3306bb41f8f126dad046845b
manishakranthi <117068020+manishakranthi@users.noreply.github.com>
2023-03-03 23:22:11 +0530

    Add files via upload

---
+++ sentiment_als.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import pickle
+import streamlit as st
+
+loaded_model = pickle.load(open('filename', 'rb'))
+
+st.title('Financial Sentiment Analysis :bar_chart:')
+
+def user_input_features():
+    Statment = st.text_input('Enter the Statment') 
+    data = {'Statment': Statment}
+    features = pd.DataFrame(data,index = [0])
+    return features 
+
+df = user_input_features()
+st.subheader('User Input parameters')
+st.write(df)
+
+prediction = loaded_model.predict(df)
+
+st.button('Click!!') 
+
+if prediction> 0:
+    st.write('The statment is Positive')
+elif prediction == 0:
+    st.write('The Statment is Neutral')
+else:
+    st.write('The Statment is Negative')
